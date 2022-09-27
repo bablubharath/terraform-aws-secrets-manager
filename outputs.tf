c3b6c858011423e701c6a0742458b5f56787db5d
Bharath <56950806+bablubharath@users.noreply.github.com>
2022-09-27 16:18:36 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -18,3 +18,48 @@ output "rotate_secret_arns" {
   description = "Rotate secret arns map"
   value       = { for k, v in aws_secretsmanager_secret.rsm : k => v["arn"] }
 }
+  
+output "this_password" {
+  value = random_password.this.result
+  sensitive = true
+}
+
+output "this_password_as_list" {
+  value = split("",random_password.this.result)
+  sensitive = true
+}
+
+output "this_password_as_json" {
+  value = jsonencode({ "password" = random_password.this.result})
+  sensitive = true
+}
+
+output "this_password_as_yaml" {
+  value = yamlencode({ "password" = random_password.this.result})
+  sensitive = true
+}
+
+output "this_password_b64" {
+  value = base64encode(random_password.this.result)
+  sensitive = true
+}
+
+output "this_password_md5" {
+  value = md5(random_password.this.result)
+  sensitive = true
+}
+
+output "this_password_sha1" {
+  value = sha1(random_password.this.result)
+  sensitive = true
+}
+
+output "this_password_sha256" {
+  value = sha256(random_password.this.result)
+  sensitive = true
+}
+
+output "this_password_sha512" {
+  value = sha512(random_password.this.result)
+  sensitive = true
+}
